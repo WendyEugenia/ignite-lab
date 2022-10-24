bd3dc2f52cbeee20c3f9be1b5e7224c655bfb2d4
WendyEugenia <wendy.t.n@hotmail.com>
2022-10-24 18:05:22 -0300

    Teste

--- aulas/lab-ds/src/App.tsx
+++ aulas/lab-ds/src/App.tsx
@@ -1,3 +1,4 @@
+import { Heading } from './components/Heading'
 import { Logo } from './logo'
 import './styles/global.css'
 
@@ -5,6 +6,11 @@ export function App() {
   return(
     <div className=' w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100' > 
     <Logo/>
+
+    <Heading size='lg'>
+      Ignite lab
+    </Heading>
+      <Text size="lg"> Faça login e comece a usar!</Text>
     </div>
     )
 }
